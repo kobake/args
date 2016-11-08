ab07dbdd8527c907f141a25949a4cb124141052d
kobake <kobake@users.sourceforge.net>
2016-11-08 11:16:12 +0900

    args

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+#include <locale.h>
+
+int main(int argc,char *argv[],char* envp[])
+{
+	setlocale(LC_ALL,"");
+
+	//カレントディレクトリ取得
+	WCHAR szCurDir[_MAX_PATH]=L"";
+	::GetCurrentDirectoryW(_countof(szCurDir),szCurDir);
+	wprintf(L"CurrentDirectory=%ls\n",szCurDir);
+
+	//引数表示
+	printf("argc=%d\n",argc);
+	for(int i=0;i<argc;i++){
+		printf("argv[%d]=%s\n",i,argv[i]);
+	}
+
+	char buf[10];
+	fgets(buf,_countof(buf),stdin);
+	return 0;
+}
